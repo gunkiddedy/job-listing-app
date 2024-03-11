15667eca64e48880e3868057ce00731300d82f79
gunkiddedy <gunkiddedy@gmail.com>
2024-03-11 23:34:12 +0700

    remove search input on detail listing

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -3,7 +3,7 @@
 {{-- @section('content') --}}
 
 <x-layout>
-    @include('partials._search')
+    {{-- @include('partials._search') --}}
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
