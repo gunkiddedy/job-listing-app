77a81faed26e5915abecfd82ad384f9492d85404
gunkiddedy <gunkiddedy@gmail.com>
2024-03-12 10:26:51 +0700

    create form listing and store to db

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,16 @@ class Listing extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'company',
+        'location',
+        'website',
+        'email',
+        'tags',
+        'description',
+    ];
+
     public function scopeFiltering($query, array $filters)
     {
         if ($filters['tag'] ?? false) {
